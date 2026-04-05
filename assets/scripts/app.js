3de0318a146023c1c2b055e8f5b39792cb0c2f11
Son, Eunhyeok <dmsgur12772@gmail.com>
2026-04-05 15:52:13 +0900

    import 및 export

---
+++ assets/scripts/app.js
@@ -0,0 +1,18 @@
+// import를 사용할 때 변수명의 대소문자 구분 잘 해야하며,
+// 파일 위치를 적을 때 자바스크립트는 확장자까지 작성해야 하며, 리액트는 확장자를 적을 피요가 없다
+import  apikey from "./util.js";
+
+// default를 사용할 때 {abc}를 안 할 경우 에러는 발생 X, 그냥 {} 없으면 default로 취급하는 것 같다.
+// default가 없을 때 {}를 사용 안하면 에러 발생
+// {}를 사용할 때 ','를 통해 한번에 여러개 import 가능 
+// 이름 뒤에 as를 붙여서 다른 별칭 지정 가능
+import {abc, def as content } from "./util.js"; 
+
+// export default를 사용했을 때 이런식으로 사용하며, 이름은 반드시 부여해야 한다.
+// 물론 default에서 {} 추가 가능하며(x/직접 해본 결과 SyntaxError 발생), 
+// {}는 여러개 넣을 때 한번에 import 가능하다.
+// import apikey from "./util.js"
+
+console.log(apikey); // 브라우저 콘솔 창에 로그를 남기는 명령어
+console.log(abc);
+console.log(content);
---
+++ assets/scripts/util.js
@@ -0,0 +1,7 @@
+// 다른 파일에서도 사용 가능하게 export 집어넣음 / 반대로 다른 파일에서 가져오려면 import를 사용한다.
+//export let apikey = "abcd1"; // api값(그냥 복습용 더미값)
+
+// export default를 사용할 때는 default는 딱 하나만 사용 가능. 물론 다른 변수 export 가능
+export default "abcd1";
+export let abc = "abc";
+export let def = "def";
